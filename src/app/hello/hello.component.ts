3b186eb847d7f81fc919999064c3bef7d37da783
jasmin-8076 <111449105+jasmin-8076@users.noreply.github.com>
2023-01-31 21:28:22 +0530

    interpolation example added

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '.app-hello',
   templateUrl: './hello.component.html',
+
   styleUrls: ['./hello.component.css']
 })
 export class HelloComponent implements OnInit {
+  
+  public name = 'thalia'
+  public port = window.location.href
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser(){
+    return "hello" + this.name
   }
 
 }
